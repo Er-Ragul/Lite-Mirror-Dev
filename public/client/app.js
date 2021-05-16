280e5c9ebc62cb18feeab53b5421ed255ff926af
Ragul.H <45266281+Er-Ragul@users.noreply.github.com>
2021-05-16 09:52:45 +0530

    Update app.js

--- public/client/app.js
+++ public/client/app.js
@@ -24,7 +24,7 @@ const startConnection = () => {
         // Working here -------------------------------------------------------------------> //
         socket.emit('makeCall', token.toString(), window.innerWidth, window.innerHeight) 
         peer = new Peer(token.toString(), {
-            host: 'localhost',
+            host: 'lite-mirror-dev.herokuapp.com',
             port: 443,
             path: '/peerjs',
             secure: true,
@@ -32,7 +32,7 @@ const startConnection = () => {
                 'iceServers': [
                        { url: 'stun:stun1.l.google.com:19302' },
                        {
-                           url: 'turn:3.131.158.239:3478?transport=udp',
+                           url: 'turn:3.19.55.3:3478?transport=udp',
                            credential: 'ragul',
                            username: 'ragul'
                        }]
