939d14eca217a433397bda7ff75d9c343f6bf201
Ragul.H <45266281+Er-Ragul@users.noreply.github.com>
2021-05-21 19:11:45 +0530

    Update source.js

--- public/engine/source.js
+++ public/engine/source.js
@@ -33,7 +33,7 @@ socket.on('YourId', (myId) => {
             'iceServers': [
                    { url: 'stun:stun1.l.google.com:19302' },
                    {
-                       url: 'turn:3.131.158.239:3478?transport=udp',
+                       url: 'turn:3.138.190.183:3478?transport=udp',
                        credential: 'ragul',
                        username: 'ragul'
                    }]
